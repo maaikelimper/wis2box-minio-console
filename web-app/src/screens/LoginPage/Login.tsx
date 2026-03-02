056290c4578d3483792c58f2ad9c1a4545b60634
Maaike <maaike.limper@gmail.com>
2026-03-02 13:57:49 +0100

    remove github-action and make change to login for test

--- web-app/src/screens/LoginPage/Login.tsx
+++ web-app/src/screens/LoginPage/Login.tsx
@@ -197,13 +197,7 @@ const Login = () => {
         }
         promoInfo={
           <span style={{ fontSize: 14, lineHeight: 1 }}>
-            MinIO is a cloud-native object store built to run on any
-            infrastructure - public, private or edge clouds. Primary use cases
-            include data lakes, databases, AI/ML, SaaS applications and fast
-            backup & recovery. MinIO is dual licensed under GNU AGPL v3 and
-            commercial license. To learn more, visit{" "}
-            <a href={"https://min.io/?ref=con"} target="_blank" rel="noopener">
-              www.min.io
+            wis2box-minio
             </a>
             .
           </span>
